f021a4a33e8ce691d74f73cbf6c64c81084cd635
Acrux <acruxstudios.dev@gmail.com>
2024-11-14 18:57:52 -0300

    Added AxDiagnosticsInspector project

---
+++ AxDiagnosticsInspector/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AxDiagnosticsInspector
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
